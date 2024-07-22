ea81fa4a6baff6296b24f4f635c544ea02a9bf7c
nadeeshadilshan <nadeeshadilshan82@gmail.com>
2024-07-22 14:33:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ if (fs.existsSync('config.env')) {
 
 const API = "https://api-aswin-sparky.koyeb.app";
 const ALIVE = process.env.ALIVE || "I am Alive";
-const HANDLERS = process.env.HANDLER || "^";
-const SESSION_ID = process.env.SESSION_ID || "";
-const SUDO = process.env.SUDO || '917012984396';
+const HANDLERS = process.env.HANDLER || ".";
+const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:8a1aac2594b30648d891c7fa09347b8d";
+const SUDO = process.env.SUDO || '94756695376';
 const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "true";
 const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "false";
 const DISABLE_PM = process.env.DISABLE_PM  || "false";
